7e0d075e1a61759c9b8e344ccfd60966c9d8166f
Austin K <40644715+AustinKr@users.noreply.github.com>
2026-06-04 22:56:29 -0700

    Add drawing functionality to grid

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 
 let gridSize = 16;
+let isMouseDown = false;
+let cellTarget = null;
+
+function tryApplyEffect(container, event)
+{
+    if(!isMouseDown)
+        return;
+    let rect = container.getBoundingClientRect();
+    let column = Math.floor((event.clientX - rect.left) / container.clientWidth * gridSize);
+    let row = Math.floor((event.clientY - rect.top) / container.clientHeight * gridSize);
+    let newCellTarget = container.children[row].children[column];
+    if(cellTarget !== null && newCellTarget === cellTarget)
+        return;
+    cellTarget = newCellTarget;
+    cellTarget.classList.toggle("hover-effect");
+}
+
 
 function main()
 {
     // TODO: Delegate events to one single listener
     
     let collapseButton = document.querySelector(".collapse-button");
-    collapseButton.addEventListener("click", () => {
+    collapseButton.addEventListener("click",
+        () => {
         collapseButton.parentElement.classList.toggle("collapesed");
         collapseButton.classList.toggle("collapesed-button");
     });
@@ -23,13 +41,22 @@ function main()
         for(let i = 0; i < gridSize; i++)
         {
             let cell = document.createElement("div");
-            cell.style.backgroundColor = "grey";
-            cell.style.flex = "1 0 auto";
-            cell.style.border = "2px solid red";
-            cell.textContent = "duh..";
+            cell.classList.add("grid-cell")
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
+
+    container.addEventListener("mousedown", (event) => {
+        if(!event.target.classList.contains("grid-cell"))
+            return;
+        isMouseDown = true;
+        tryApplyEffect(container, event);
+    });
+    container.addEventListener("mouseup", () => {
+        isMouseDown = false;
+        cellTarget = null;
+    });
+    container.addEventListener("mousemove", event => tryApplyEffect(container, event));
 }
 main();
