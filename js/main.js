087556273ada0cdcf356cd0f3fc5d81094297435
Austin K <40644715+AustinKr@users.noreply.github.com>
2026-06-04 20:33:11 -0700

    Add grid of divs

--- js/main.js
+++ js/main.js
@@ -4,11 +4,28 @@ function toggleCollapseParent(element)
     element.parentElement.classList.toggle("collapesed");
 }
 
+let gridSize = 16;
+
 function main()
 {
+    // TODO: Delegate events to one single listener
+    
     let collapseButton = document.querySelector(".collapse-button");
     collapseButton.addEventListener("click", event => {
         toggleCollapseParent(event.target);
     });
+
+    let container = document.querySelector(".grid-container");
+    let cellSize = container.offsetWidth / gridSize;
+    for(let i = 0; i < gridSize*gridSize; i++)
+    {
+        let cell = document.createElement("div");
+        cell.style.backgroundColor = "grey";
+        cell.style.width = cellSize;
+        cell.style.height = cellSize;
+        cell.style.flex = "0 0 auto";
+        cell.textContent = "duh..";
+        container.appendChild(cell);
+    }
 }
 main();
